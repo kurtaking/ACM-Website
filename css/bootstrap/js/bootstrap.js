dd53b355e9f3068f151773845fa0d38aed139aeb
Christopher Doege <cdminigun@gmail.com>
2015-12-15 00:03:58 -0600

    Updated css for navbar. Fixed Calendar.

--- css/bootstrap/js/bootstrap.js
+++ css/bootstrap/js/bootstrap.js
@@ -5,8 +5,8 @@
  */
 
 /*!
- * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=cbccff75143f385adef3)
- * Config saved to config.json and https://gist.github.com/cbccff75143f385adef3
+ * Generated using the Bootstrap Customizer (http://getbootstrap.com/customize/?id=2d8e3b5b093bf8b81d96)
+ * Config saved to config.json and https://gist.github.com/2d8e3b5b093bf8b81d96
  */
 if (typeof jQuery === 'undefined') {
   throw new Error('Bootstrap\'s JavaScript requires jQuery')
